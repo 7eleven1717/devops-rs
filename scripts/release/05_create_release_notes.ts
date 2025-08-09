b32c25f7423b027231d35eef4391c61d87923075
7eleven1717 <194344232+7eleven1717@users.noreply.github.com>
2025-08-09 16:14:47 +0900

    [ci skip]chore: some changes

--- scripts/release/05_create_release_notes.ts
+++ scripts/release/05_create_release_notes.ts
@@ -1,4 +1,10 @@
 #!/usr/bin/env -S deno run -A
 
 // This script reffers to the original Deno script for creating tag.
-// https://github.com/denoland/deno/blob/c86e277fc1e0795f3602981e36c690e1abfbfe49/tools/release/04_post_publish.ts
+// https://github.com/denoland/deno/blob/c86e277fc1e0795f3602981e36c690e1abfbfe49/tools/release/05_create_release_notes.ts
+
+import * as core from "@actions/core";
+
+const changeLog = await Deno.readTextFile("./CHANGELOG.md");
+const body = changeLog.slice(0, changeLog.indexOf("\n## "));
+core.setOutput("body", body);
