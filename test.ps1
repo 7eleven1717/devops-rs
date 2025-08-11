e8a1759f8c158153cce30f25d94962c1b03deae3
7eleven <194344232+7eleven1717@users.noreply.github.com>
2025-08-11 14:34:08 +0900

    chore: delete test.ps1 test.sh

--- test.ps1
+++
@@ -1,6 +0,0 @@
-foreach ($item in Get-ChildItem -Recurse -Depth 1) {
-  $filename = "$($item.Name)-foo.zip"
-  # Compress-Archive -Path .\sample\* -DestinationPath .\sample.zip
-  Compress-Archive -Path $item.Name -DestinationPath $filename
-  Get-FileHash $name -Algorithm SHA256
-}
--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-for file in $(find . type -f); do
-    basename
-done
